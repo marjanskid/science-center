7f77d57f99ec3a0a07426fc439362de9f2e71748
marjanskid <marjanskid@uns.ac.rs>
2020-01-03 02:02:45 +0100

    test commit

---
+++ src/main/java/dm/sciencecenter/ScienceCenterApplication.java
@@ -0,0 +1,13 @@
+package dm.sciencecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScienceCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScienceCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dm/sciencecenter/ScienceCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package dm.sciencecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScienceCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
